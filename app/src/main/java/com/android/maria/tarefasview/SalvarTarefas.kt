e270b12de43ecf140a6a1784a2115d802ee6c35d
Felipe <feliipe.romaoo@gmail.com>
2024-05-21 22:44:21 -0300

    Começando a criar a tela de salvar tarefas

--- app/src/main/java/com/android/maria/tarefasview/SalvarTarefas.kt
+++ app/src/main/java/com/android/maria/tarefasview/SalvarTarefas.kt
@@ -1,9 +1,42 @@
 package com.android.maria.tarefasview
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.android.maria.ui.theme.White
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SalvarTarefas(navController: NavController){
+    androidx.compose.material.Scaffold(
+        topBar = {
+            androidx.compose.material.TopAppBar(
+                backgroundColor = White,
+                title = {
+                    Text(
+                        text = "Salvar Tarefa",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = White)
+                }
+            )
+        }
+    ){
+       Column(
+           modifier = Modifier
+               .fillMaxSize()
+               .verticalScroll(rememberScrollState())
+       ) {
 
+       }
+    }
 }
